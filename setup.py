671e683b7d0b8c20debb7ac0b71fc1191cfb426b
alaric <alaric@wullerot.ch>
2016-10-18 15:24:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name="django-cms-plugins",
-    version='0.1.4',
+    version='0.1.5',
     url='http://github.com/rouxcode/django-cms-plugins',
     license='MIT',
     platforms=['OS Independent'],
